bffd2d19ef96eea9764489d9cd151f7cfcef0321
kod0406 <kod04061@gmail.com>
2024-01-09 17:20:49 +0900

    Todo 리스트 예제
    Jsp와 서블릿 복습

---
+++ 스프링 연습/1장/src/main/java/org/zerock/java_spring/Todo/service/TodoReadController.java
@@ -0,0 +1,31 @@
+package org.zerock.java_spring.Todo.service;
+
+import org.zerock.java_spring.Todo.dto.TodoDTO;
+import org.zerock.java_spring.Todo.service.TodoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "todoReadController", urlPatterns = "/todo/read")
+public class TodoReadController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("/todo/read");
+
+        // /todo/read?tno=123
+        Long tno = Long.parseLong(req.getParameter("tno"));
+
+        TodoDTO dto = TodoService.INSTANCE.get(tno);
+
+        req.setAttribute("dto", dto);
+
+        req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req,resp);
+    }
+
+}
---
+++ 스프링 연습/1장/src/main/java/org/zerock/java_spring/calc/CalcController.java
@@ -0,0 +1,20 @@
+package org.zerock.java_spring.calc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name ="CalcController",urlPatterns = "/calc/makeResult")
+public class CalcController extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+        String num1 =req.getParameter("num1");
+        String num2 =req.getParameter("num2");
+        System.out.printf(" num1: %s",num1);
+        System.out.printf(" num2: %s",num1);
+        resp.sendRedirect("/index");
+    }
+}
