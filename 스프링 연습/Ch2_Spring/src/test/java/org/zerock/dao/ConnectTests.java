c4afa81fed3bb6d0c45af38174b11e4d43f71989
kod0406 <kod04061@gmail.com>
2024-01-10 18:40:49 +0900

    2장 일부 커밋
    오류를 못 찾겠어서 처음부터 다시짜는 중

---
+++ 스프링 연습/Ch2_Spring/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,12 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class ConnectTests {
+    @Test
+    public void test1() {
+    int v1=10;
+    int v2=10;
+    Assertions.assertEquals(v1,v2);
+    }
+}
