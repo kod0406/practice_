5bece635b43b989925c2140d8dcb1c125c75d74c
kod0406 <kod04061@gmail.com>
2024-01-15 17:05:59 +0900

    2장 정리
    웹 MVC구조 학습

--- 스프링 연습/Ch2_Spring/src/main/java/org/zerock/ch2_spring/controller/TodoListController.java
+++ 스프링 연습/Ch2_Spring/src/main/java/org/zerock/ch2_spring/controller/TodoListController.java
@@ -2,13 +2,14 @@ package org.zerock.ch2_spring.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.zerock.ch2_spring.service.TodoService;
+import org.zerock.ch2_spring.dto.TodoDTO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.util.List;
 import java.io.IOException;
 
 @WebServlet(name="todoListController",value = "/todo/list")
@@ -17,6 +18,15 @@ public class TodoListController extends HttpServlet {
     private TodoService todoService = TodoService.INSTANCE;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
-    log.info("todo list.........");}
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
+        log.info("todo list.........");
+        try {
+            List<TodoDTO> dtoList = todoService.listAll();
+            req.setAttribute("dtoList",dtoList);
+            req.getRequestDispatcher("WEB-INF/todo/list.jsp").forward(req,resp);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            throw new ServletException("list error");
+        }
+    }
 }
--- 스프링 연습/Ch2_Spring/src/main/java/org/zerock/ch2_spring/service/TodoService.java
+++ 스프링 연습/Ch2_Spring/src/main/java/org/zerock/ch2_spring/service/TodoService.java
@@ -22,13 +22,38 @@ public enum TodoService {
         modelMapper = MapperUtil.INSTANCE.get();
     }
 
-    public void register(TodoDTO todoDTO)throws Exception {
+    public void register(TodoDTO todoDTO) throws Exception {
 
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
 
-       //System.out.println("todoVO: " + todoVO);
+        //System.out.println("todoVO: " + todoVO);
         log.info(todoVO);
 
         dao.insert(todoVO); //int 를 반환하므로 이를 이용해서 예외처리도 가능
     }
+
+    public List<TodoDTO> listAll() throws Exception {
+        List<TodoVO> voList = dao.selectAll();
+        log.info("voList..........");
+        log.info(voList);
+        List<TodoDTO> dtoList = voList.stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+        return dtoList;
     }
+    public TodoDTO get(Long tno)throws Exception{
+        log.info("tno: "+ tno);
+        TodoVO todoVO = dao.selectOne(tno);
+        TodoDTO todoDTO = modelMapper.map(todoVO,TodoDTO.class);
+        return todoDTO;
+    }
+    public void remove(Long tno)throws Exception{
+        log.info("tno: " + tno);
+        dao.deleteOne(tno);
+    }
+    public void modify(TodoDTO todoDTO)throws Exception{
+        log.info("todoDTO: "+ todoDTO);
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+        dao.updateOne(todoVO);
+    }
+}
