c3c488a68c44b5ee044d0a0e3c402b7d2352660c
kod0406 <kod04061@gmail.com>
2024-02-11 19:38:54 +0900

    Create interface.cs
    C#인터페이스 연습

---
+++ C 연습/interface.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Metrics;
+using System.Security.Claims;
+
+interface IPropertyCounter
+{
+    int Count { get; set; }
+}
+
+interface IMethodCounter
+{
+    int Count(int i);
+}
+interface ICounter : IProperty Counter, IMethodCounter { }
+
+Class InterfaceParameter
+{
+void Test(ICounter x) {
+        ((IMethodCounter)x).Count(1);
+        ((IPropertyCounter)x).Count = 1;
+    }
+
